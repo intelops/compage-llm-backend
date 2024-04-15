850614265562164717d92e9993da1d5e6108b4c9
MrAzharuddin <azharuddinmohammed998@gmail.com>
2024-04-15 19:34:22 +0530

    Refactor database connection handling and improve app shutdown process

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ async def lifespan(_: FastAPI):
     db_session = get_session()
     if not db_session:
         logger.error("Database connection failed!")
+        yield None
         return
     logger.info("Database connected!")
-    yield
+    try:
+        yield db_session
+    finally:
+        db_session.close()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -39,4 +43,4 @@ app.include_router(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
--- pkg/src/config/database.py
+++ pkg/src/config/database.py
@@ -1,27 +1,31 @@
-""" Module providing a function returning a cassandra session. """
-
-from cassandra.cluster import Cluster  # pylint: disable = no-name-in-module
+from cassandra.cluster import Cluster, NoHostAvailable, NoConnectionsAvailable # pylint: disable = no-name-in-module
 from cassandra.policies import DCAwareRoundRobinPolicy
 from cassandra.cqlengine.connection import register_connection, set_default_connection
 
 KEYSPACE = "backend_llm"
 
-
 def get_session():
     """
     A function to get the cluster and establish a session to a specific keyspace.
+    Includes exception handling for cases where the cluster is not available.
     """
-    cluster = Cluster(
-        ["127.0.0.1"],
-        port=9042,
-        load_balancing_policy=DCAwareRoundRobinPolicy(local_dc="datacenter1"),
-        protocol_version=4,
-    )
-    session = cluster.connect(keyspace=KEYSPACE, wait_for_all_pools=True)
-    register_connection(str(session), session=session)
-    set_default_connection(str(session))
-    return session
-
+    try:
+        cluster = Cluster(
+            ["127.0.0.1"],
+            port=9042,
+            load_balancing_policy=DCAwareRoundRobinPolicy(local_dc="datacenter1"),
+            protocol_version=4,
+        )
+        session = cluster.connect(keyspace=KEYSPACE, wait_for_all_pools=True)
+        register_connection(str(session), session=session)
+        set_default_connection(str(session))
+        return session
+    except NoHostAvailable:
+        return None
+    except NoConnectionsAvailable:
+        return None
+    except ConnectionRefusedError:
+        return None
 
 def get_keyspace() -> str:
     """
