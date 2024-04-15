ccfbd1d1c6a6a083bfd9ad41b0a3119fd4d71316
Azharuddin Mohammed <42718729+MrAzharuddin@users.noreply.github.com>
2024-04-15 19:42:48 +0530

    Refactor db (#21)
    * enhancement: removed unused methods
    
    * refactor: added cassandra locally, and restructured codebase, removed user auth management
    
    * chore: code formatted
    
    * docs: update README.md
    
    * chore: updated requirements file
    
    * Refactor database connection handling and improve error management
    
    ---------
    
    Signed-off-by: Azharuddin Mohammed <42718729+MrAzharuddin@users.noreply.github.com>

--- pkg/src/config/database.py
+++ pkg/src/config/database.py
@@ -1,3 +1,5 @@
+
+from cassandra.cluster import Cluster, NoHostAvailable, NoConnectionsAvailable # pylint: disable = no-name-in-module
 """ Module providing a function returning a cassandra session. """
 
 from cassandra.cluster import Cluster  # pylint: disable = no-name-in-module
@@ -6,10 +8,28 @@ from cassandra.cqlengine.connection import register_connection, set_default_conn
 
 KEYSPACE = "backend_llm"
 
-
 def get_session():
     """
     A function to get the cluster and establish a session to a specific keyspace.
+    Includes exception handling for cases where the cluster is not available.
+    """
+    try:
+        cluster = Cluster(
+            ["127.0.0.1"],
+            port=9042,
+            load_balancing_policy=DCAwareRoundRobinPolicy(local_dc="datacenter1"),
+            protocol_version=4,
+        )
+        session = cluster.connect(keyspace=KEYSPACE, wait_for_all_pools=True)
+        register_connection(str(session), session=session)
+        set_default_connection(str(session))
+        return session
+    except NoHostAvailable:
+        return None
+    except NoConnectionsAvailable:
+        return None
+    except ConnectionRefusedError:
+        return None
     """
     cluster = Cluster(
         ["127.0.0.1"],
@@ -21,7 +41,6 @@ def get_session():
     register_connection(str(session), session=session)
     set_default_connection(str(session))
     return session
-
 
 def get_keyspace() -> str:
     """
