cd552eeee095a5dfd34433e76bbb277afb501b62
Azharuddin Mohammed <42718729+MrAzharuddin@users.noreply.github.com>
2024-04-16 13:13:46 +0530

    chore: bug fixes according to deepsource (#22)
    Refactor: database configuration imports and remove redundant session creation logic in `database.py`

--- pkg/src/config/database.py
+++ pkg/src/config/database.py
@@ -1,8 +1,11 @@
+from cassandra.cluster import (
+    Cluster,
+    NoHostAvailable,
+    NoConnectionsAvailable,
+)  # pylint: disable = no-name-in-module
 
 from cassandra.cluster import Cluster, NoHostAvailable, NoConnectionsAvailable # pylint: disable = no-name-in-module
 """ Module providing a function returning a cassandra session. """
-
-from cassandra.cluster import Cluster  # pylint: disable = no-name-in-module
 from cassandra.policies import DCAwareRoundRobinPolicy
 from cassandra.cqlengine.connection import register_connection, set_default_connection
 
@@ -30,17 +33,7 @@ def get_session():
         return None
     except ConnectionRefusedError:
         return None
-    """
-    cluster = Cluster(
-        ["127.0.0.1"],
-        port=9042,
-        load_balancing_policy=DCAwareRoundRobinPolicy(local_dc="datacenter1"),
-        protocol_version=4,
-    )
-    session = cluster.connect(keyspace=KEYSPACE, wait_for_all_pools=True)
-    register_connection(str(session), session=session)
-    set_default_connection(str(session))
-    return session
+
 
 def get_keyspace() -> str:
     """
